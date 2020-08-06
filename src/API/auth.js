63816b1a8a77e354bc11ef8a528c43507740c1dd
Stone Hobbs <stone@minnykid.com>
2020-08-05 19:28:53 -0500

    finished basics for login

--- src/API/auth.js
+++ src/API/auth.js
@@ -2,25 +2,15 @@ import axios from 'axios'
 import runtimeEnv from '@mars/heroku-js-runtime-env'
 
 const env = runtimeEnv()
-//const apiRoot = 'http://localhost:5003/graphql'
 const apiRoot = env.REACT_APP_API
 
 export default class AuthAPI {
 
   async login(email, password) {
-    const query = {
-      query: `
-        query {
-          login(email: "${email}", password: "${password}"){
-            userId
-            token
-            tokenExpiration
-          }
-        }
-      `
-    }
-
-    const res = await axios.post(apiRoot, query)
-    return res.data.data.login
+    const user = await axios.post(apiRoot + '/auth/login', {
+      email,
+      password
+    });
+    return user.data;
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import 'primereact/resources/themes/nova-colored/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import './App.css';
 
 import AuthPage from './Pages/Auth';
+import MenuPage from './Pages/Menu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AuthPage />
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <Switch>
+            {this.props.token && <Redirect from="/auth" to="/menu" exact />}
+            {!this.props.token && <Route path="/auth" component={AuthPage} />}
+            {!this.props.token && <Redirect to="/auth" exact />}
+            {<Route path="/menu" component={MenuPage} />}
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  userStatus: state.auth.userStatus,
+  userId: state.auth.userId,
+  token: state.auth.token,
+  tokenExpiration: state.auth.tokenExpiration
+})
+
+export default connect(mapStateToProps)(App)
